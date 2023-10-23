8c86c601f6c18037cb4c0158414bf2f49808a4da
bp442 <bpark135@gmail.com>
2023-10-23 11:40:22 -0400

    fixed mortgageCalculator totalInterest calculation

--- scripts/CDCalculator.js
+++ scripts/CDCalculator.js
@@ -3,21 +3,20 @@
 window.onload = init;
 
 function init() {
-    const calculateBtn = document.getElementById("calculateMortgageBtn");
-    calculateBtn.onclick = calculatePayments;
+    const calculateBtn = document.getElementById("calculateValueBtn");
+    calculateBtn.onclick = calculateValue;
 }
 
 
-function calculatePayments() {
+function calculateValue() {
     const principal = Number(document.getElementById("principalText").value);
     const interestRate = Number(document.getElementById("interestRateText").value) / 100; //converts it into a decimal rate
     const loanLengthYears = Number(document.getElementById("loanLengthText").value);
 
-    var monthlyPayment = (principal * (interestRate/12) * (Math.pow((1 + (interestRate/12)), (loanLengthYears * 12)) / 
-    ((Math.pow((1 + (interestRate/12)), (loanLengthYears * 12)) - 1))));
-    const totalInterest = (Math.round(((monthlyPayment * (loanLengthYears * 12)) - principal)*100)/100).toFixed(2);
+    const monthlyPayment = (Math.round((principal * (interestRate/12) * (Math.pow((1 + (interestRate/12)), (loanLengthYears * 12)) / 
+    ((Math.pow((1 + (interestRate/12)), (loanLengthYears * 12)) - 1))))*100)/100).toFixed(2)
 
-    monthlyPayment = (Math.round(monthlyPayment * 100)/100);
+    const totalInterest = (Math.round(((monthlyPayment * (loanLengthYears * 12)) - principal)*100)/100).toFixed(2);
 
     const answer = "Your monthly payments will be $" + monthlyPayment + " and your total interest paid will be $" + totalInterest + ".";
     document.getElementById("monthlyPayAndTotalInterest").value = answer;
